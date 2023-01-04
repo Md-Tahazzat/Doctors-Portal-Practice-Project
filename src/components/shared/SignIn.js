98964a40ba0c946f53bf2d5df2a09a0ba81a50c9
Md-Tahazzat <mdtahazzatali820@gmail.com>
2023-01-04 16:59:25 +0600

    completed style of login page

--- src/components/shared/SignIn.js
+++ src/components/shared/SignIn.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./SignIn.css";
 
 const SignIn = () => {
+  const [visible, setVisible] = useState("password");
+  const handleInputType = () => {
+    setVisible((curr) => (curr === "password" ? "text" : "password"));
+  };
   return (
-    <div className="h-screen w-full md:w-3/6 lg:w-2/6 mx-auto mt-16 login-form">
-      <form className="px-5 py-5 md:border lg:border bg-slate-50 dark:bg-slate-700 lg:border-slate-400 rounded-lg lg:shadow-lg">
-        <h1 className="text-center text-3xl lg:text-4xl my-5 font-bold text-blue-700 dark:text-secondary">
+    <div className="login-form mx-auto mt-16 box-border h-screen w-full md:w-3/6 lg:w-2/6">
+      <form className="rounded-lg bg-slate-50 px-5 py-5 dark:bg-slate-700 md:border lg:border lg:border-slate-400 lg:shadow-lg">
+        <h1 className="my-5 text-center text-3xl font-bold text-blue-700 dark:text-secondary lg:text-4xl">
           Please SignIn
         </h1>
         <div className="input-box pt-5">
           <input
-            className="py-2 px-4 w-full bg-transparent focus:border-2 focus:border-blue-700 dark:focus:border-blue-300 text-xl border rounded-md focus:outline-none "
+            className="w-full rounded-md border bg-transparent py-2 px-4 text-xl focus:border-2 focus:border-blue-700 focus:outline-none dark:focus:border-blue-300 "
             autoComplete="off"
             type="text"
             required
@@ -25,21 +29,33 @@ const SignIn = () => {
           </label>
         </div>
         <div className="input-box">
+          <span
+            onClick={handleInputType}
+            className="password-show relative top-[2.2rem] right-3 z-10 float-right cursor-pointer"
+          >
+            {visible === "password" ? (
+              <i className="fa-sharp fa-solid fa-eye-slash"></i>
+            ) : (
+              <i className="fa-solid fa-eye"></i>
+            )}
+          </span>
           <input
-            className="py-2 px-4 w-full bg-transparent focus:border-2 focus:border-blue-700 dark:focus:border-blue-300 text-xl border rounded-md focus:outline-none "
+            className="w-full rounded-md border bg-transparent py-2 px-4 text-xl focus:border-2 focus:border-blue-700 focus:outline-none dark:focus:border-blue-300 "
             autoComplete="off"
-            type="password"
+            type={visible}
             name="password"
             required
           />
+
           <label
             htmlFor="password"
             className="input-label bg-slate-50 dark:bg-slate-700 dark:text-white"
           >
             Password
           </label>
+
           <Link
-            className="block mt-[-20px] w-32 mb-5 text-blue-700 dark:text-secondary hover:underline "
+            className="mt-[-20px] mb-5 block w-32 text-blue-700 hover:underline dark:text-secondary "
             to="forgotPassword"
           >
             forgot password?
@@ -48,18 +64,33 @@ const SignIn = () => {
 
         <div className="pt-5 pb-10">
           <NavLink
-            to="/register"
+            to="/signUp"
             className="float-left font-bold text-blue-600 hover:text-blue-900 dark:text-secondary dark:hover:text-blue-400"
           >
             Create account
           </NavLink>
           <input
             type="submit"
-            className="float-right mr-10 py-1 px-3 bg-blue-500 rounded text-white hover:bg-blue-600 lg:text-2xl"
+            className="hover:bg-blue-00 float-right mr-1 rounded bg-primary py-1 px-3 text-white md:mr-10 lg:mr-10  lg:text-2xl"
             value="SignIn"
           />
         </div>
       </form>
+      <div className="divider my-5 mx-5">OR</div>
+
+      {/* social sign in section. */}
+      <button className="text-md my-3 mx-auto block w-[95%] rounded-lg border-2 bg-slate-100 py-2 font-bold text-slate-700 shadow-md hover:bg-slate-200 dark:bg-slate-700 dark:text-white dark:hover:border-secondary dark:hover:bg-slate-600 md:mx-auto md:w-5/6 md:text-2xl lg:mx-auto lg:w-5/6 lg:text-2xl">
+        <i className="fa-brands fa-google mx-1 text-2xl md:mx-2 md:text-3xl lg:mx-2 lg:text-3xl"></i>
+        CONTINUE WITH GOOGLE
+      </button>
+      <button className="text-md my-3 mx-auto block w-[95%]  rounded-lg border-2 bg-slate-100 py-2  font-bold text-slate-700 shadow-md hover:bg-slate-200 dark:bg-slate-700 dark:text-white dark:hover:border-secondary dark:hover:bg-slate-600 md:mx-auto md:w-5/6 md:text-2xl lg:mx-auto lg:w-5/6 lg:text-2xl">
+        <i className="fa-brands fa-facebook mx-1 text-2xl md:mx-2 md:text-3xl lg:mx-2 lg:text-3xl"></i>
+        CONTINUE WITH FACEBOOK
+      </button>
+      <button className="text-md my-3 mx-auto block w-[95%] rounded-lg border-2 bg-slate-100 py-2 font-bold text-slate-700 shadow-md hover:bg-slate-200 dark:bg-slate-700 dark:text-white dark:hover:border-secondary dark:hover:bg-slate-600 md:mx-auto md:w-5/6 md:text-2xl lg:mx-auto lg:w-5/6 lg:text-2xl">
+        <i className="fa-brands fa-github mx-1 text-2xl md:mx-2 md:text-3xl lg:mx-2 lg:text-3xl"></i>
+        CONTINUE WITH GITHUB
+      </button>
     </div>
   );
 };
--- src/components/shared/SignUp.js
+++ src/components/shared/SignUp.js
@@ -1,9 +1,88 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div>
-      <h1>SignUp</h1>
+    <div className="login-form mx-auto mt-8 h-screen w-full md:w-3/6 lg:w-2/6">
+      <form className="rounded-lg bg-slate-50 px-5 py-5 dark:bg-slate-700 md:border lg:border lg:border-slate-400 lg:shadow-lg">
+        <h1 className="my-5 text-center text-3xl font-bold text-blue-700 dark:text-secondary lg:text-4xl">
+          Please SignUp
+        </h1>
+        <div className="input-box pt-5">
+          <input
+            className="w-full rounded-md border bg-transparent py-2 px-4 text-xl focus:border-2 focus:border-blue-700 focus:outline-none dark:focus:border-blue-300 "
+            autoComplete="off"
+            type="text"
+            required
+            name="text"
+          />
+          <label
+            htmlFor="text"
+            className="input-label bg-slate-50 dark:bg-slate-700 dark:text-white"
+          >
+            Email
+          </label>
+        </div>
+        <div className="input-box">
+          <input
+            className="w-full rounded-md border bg-transparent py-2 px-4 text-xl focus:border-2 focus:border-blue-700 focus:outline-none dark:focus:border-blue-300 "
+            autoComplete="off"
+            type="password"
+            name="password"
+            required
+          />
+          <label
+            htmlFor="password"
+            className="input-label bg-slate-50 dark:bg-slate-700 dark:text-white"
+          >
+            Password
+          </label>
+        </div>
+        <div className="input-box">
+          <input
+            className="w-full rounded-md border bg-transparent py-2 px-4 text-xl focus:border-2 focus:border-blue-700 focus:outline-none dark:focus:border-blue-300 "
+            autoComplete="off"
+            type="password"
+            name="confirm-password"
+            required
+          />
+          <label
+            htmlFor="confirm-password"
+            className="input-label bg-slate-50 dark:bg-slate-700 dark:text-white"
+          >
+            Confirm Password
+          </label>
+        </div>
+
+        <div className="pt-5 pb-10">
+          <span className="float-left">Already have an account?</span>
+          <NavLink
+            to="/signIn"
+            className="mx-2 font-bold text-blue-600 hover:text-blue-900 dark:text-primary dark:hover:text-blue-400"
+          >
+            SignIn
+          </NavLink>
+          <input
+            type="submit"
+            className="hover:bg-blue-00 float-right mr-1 rounded bg-primary py-1 px-3 text-white md:mr-10 lg:mr-10  lg:text-2xl"
+            value="SignUp"
+          />
+        </div>
+      </form>
+      <div className="divider my-5 mx-5">OR</div>
+      {/* social sign in section. */}
+      <button className="text-md my-3 mx-auto block w-[95%] rounded-lg border-2 bg-slate-100 py-2 font-bold text-slate-700 shadow-md hover:bg-slate-200 dark:bg-slate-700 dark:text-white dark:hover:border-secondary dark:hover:bg-slate-600 md:mx-auto md:w-5/6 md:text-2xl lg:mx-auto lg:w-5/6 lg:text-2xl">
+        <i className="fa-brands fa-google mx-1 text-2xl md:mx-2 md:text-3xl lg:mx-2 lg:text-3xl"></i>
+        CONTINUE WITH GOOGLE
+      </button>
+      <button className="text-md my-3 mx-auto block w-[95%]  rounded-lg border-2 bg-slate-100 py-2  font-bold text-slate-700 shadow-md hover:bg-slate-200 dark:bg-slate-700 dark:text-white dark:hover:border-secondary dark:hover:bg-slate-600 md:mx-auto md:w-5/6 md:text-2xl lg:mx-auto lg:w-5/6 lg:text-2xl">
+        <i className="fa-brands fa-facebook mx-1 text-2xl md:mx-2 md:text-3xl lg:mx-2 lg:text-3xl"></i>
+        CONTINUE WITH FACEBOOK
+      </button>
+      <button className="text-md my-3 mx-auto block w-[95%] rounded-lg border-2 bg-slate-100 py-2 font-bold text-slate-700 shadow-md hover:bg-slate-200 dark:bg-slate-700 dark:text-white dark:hover:border-secondary dark:hover:bg-slate-600 md:mx-auto md:w-5/6 md:text-2xl lg:mx-auto lg:w-5/6 lg:text-2xl">
+        <i className="fa-brands fa-github mx-1 text-2xl md:mx-2 md:text-3xl lg:mx-2 lg:text-3xl"></i>
+        CONTINUE WITH GITHUB
+      </button>
     </div>
   );
 };
